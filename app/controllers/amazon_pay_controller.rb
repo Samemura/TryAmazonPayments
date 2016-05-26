c9b05ebe5936f45ec428e28b22a0b401932def10
murano-masaki <murano-masaki@kayac.com>
2016-05-26 17:57:09 +0900

    remove binding.

--- app/controllers/amazon_pay_controller.rb
+++ app/controllers/amazon_pay_controller.rb
@@ -21,7 +21,6 @@ class AmazonPayController < ApplicationController
     amazon_order_reference_id = params[:order_reference_id]
     address_consent_token = access_token
 
-    binding.pry
     amazon_client.set_order_reference_details(
       amazon_order_reference_id,
       10000,
@@ -30,10 +29,6 @@ class AmazonPayController < ApplicationController
       store_name: 'Amazon pay store'
     )
 
-    # Make the ConfirmOrderReference API call to
-    # confirm the details set in the API call
-    # above.
-    amazon_order_reference_id = 'AMAZON_ORDER_REFERENCE_ID'
     amazon_client.confirm_order_reference(amazon_order_reference_id)
   end
 
